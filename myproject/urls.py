07fdcaae61dbde1f491dbdd432d5ef9bfeb66cd6
Christopher Adams <christopher.r.adams@gmail.com>
2024-09-21 12:20:32 +0000

    Upgraded old url() to path()

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, url, include
+from django.urls import path, re_path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
@@ -8,15 +8,15 @@ from blog import views
 admin.autodiscover()
 
 
-urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', views.blog_list, name="blog_list"),
-    url(r'^blog/(?P<blog_id>[0-9]+)/$', views.blog_detail, name="blog_detail"),
-)
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.blog_list, name="blog_list"),
+    re_path(r'^blog/(?P<blog_id>[0-9]+)/$', views.blog_detail, name="blog_detail"),
+]
 
 # Serve debug toolbar.
 if settings.DEBUG:
     import debug_toolbar
-    urlpatterns += patterns('',
-        url(r'^__debug__/', include(debug_toolbar.urls)),  # NOQA
-    )
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),  # NOQA
+    ]
